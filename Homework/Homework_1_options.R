b317badfb99fae50beabfd3433213d2e5b8fdc3e
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-09-22 15:09:01 +0800

    Create Homework_1_options.R

---
+++ Homework/Homework_1_options.R
@@ -0,0 +1,18 @@
+options()$repos
+options()$BioC_mirror
+options(bio_mirror="https://mirrors.ustc.edu.cn/bioc/")
+options("repos"=c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))
+
+
+
+
+
+# > options()$repos
+#                                 CRAN                            CRANextra 
+#           "http://cran.rstudio.com/" "http://www.stats.ox.ac.uk/pub/RWin" 
+# attr(,"RStudio")
+# [1] TRUE
+# > options()$BioC_mirror
+# NULL
+# > options(bio_mirror="https://mirrors.ustc.edu.cn/bioc/")
+# > options("repos"=c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))
