e205f8830b8cd324e1e0eb7faa2c14d7f173c3e2
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-10-27 15:11:46 +0800

    Create Code.R

---
+++ Curve/Code.R
@@ -0,0 +1,5 @@
+curve(dnorm(x,0,1), xlim=c(-5,5), ylim=c(0,.8),col='red', lwd=2, lty=3)
+curve(dnorm(x,0,2),add= T, col='blue',lwd=2,lty=2)
+curve(dnorm(x,0,0.5),add=T,lwd=2,lty=1)
+title(main="Gaussian distributions")
+legend(par('usr')[2],par('usr')[4],xjust=1,c('sigma=1','sigma=2','sigma=1/2'),lwd=c(2,2,2),lty=c(3,2,1),col=c('red','blue',par('fg')))
